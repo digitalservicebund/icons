1b7140c296f2076ca049aa68bde78eb6ebf67fab
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2024-01-05 11:41:09 +0100

    feat: add additional default properties to root svg element
    Co-authored-by: chris <christoph.hohnerlein@digitalservice.bund.de>

---
+++ template.js
@@ -0,0 +1,27 @@
+const template = (variables, { tpl }) => {
+  variables.jsx.openingElement.attributes.push({
+    "type": "JSXAttribute",
+    "name": {
+      "type": "JSXIdentifier",
+      "name": "data-testid"
+    },
+    "value": {
+      "type": "StringLiteral",
+      "value": `${variables.componentName.replace("Svg", "")}Icon`
+    }
+  })
+
+  return tpl`
+${variables.imports};
+
+${variables.interfaces};
+
+const ${variables.componentName} = (${variables.props}) => (
+  ${variables.jsx}
+);
+
+${variables.exports};
+`
+}
+
+module.exports = template
